05bd849e889ef4ff54f10a7c53367434b29d0da6
Luiko <lokoluis_box@hotmail.com>
2018-02-13 09:34:15 -0700

    lifting timer state up

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 
-const START = 'Start';
-const PAUSE = 'Pause';
-const CONTINUE = 'Continue';
-let timer;
-
 function twoDigPadding(dig) {
   return dig.toString().length === 1? `0${dig}`: dig;
 }
@@ -20,78 +15,22 @@ function convertStoM(seconds) {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: START,
-      dayHours: 0,
-      weekHours: 0,
-      remainingTime: 0
-    };
-    this.click = this.click.bind(this);
-    this.setTimer = this.setTimer.bind(this);
-  }
-
-  setTimer() {
-    let start = Date.now();
-    timer = setInterval(function () {
-      if (this.state.remainingTime > 0) {
-        const now = Date.now();
-        this.setState(prevState => ({ 
-          remainingTime: prevState.remainingTime - Math.floor((now - start) / 1000)
-        }));
-        start = now;
-      }
-    }.bind(this), 1000);
-  }
-
-  click() {
-    const { name } = this.state;
-    if (name === START) {
-      this.setState({ name: PAUSE, remainingTime: 60 * 60 });
-      this.setTimer();
-    } else if (name === PAUSE) {
-      this.setState({ name: CONTINUE });
-      clearInterval(timer);
-    } else if (name === CONTINUE) {
-      this.setState({ name: PAUSE });
-      this.setTimer();
-    } else {
-      throw 'This error should never happend';
-    }
   }
 
   componentDidUpdate() {
-    const { name, remainingTime } = this.state;
-    const aHour = 60 * 60;
-    if (name === PAUSE && remainingTime === 0) {
-      this.setState(function (prevState) {
-        return {
-          remainingTime: aHour,
-          dayHours: prevState.dayHours + 1,
-          weekHours: prevState.weekHours + 1
-        };
-      });
-    } else if (name === PAUSE && remainingTime < 0) {
-      this.setState(function (prevState) {
-        const hourPassed = Math.floor((-1 * prevState.remainingTime) / aHour);
-        return {
-          remainingTime: hourPassed? aHour * hourPassed + remainingTime: aHour + remainingTime,
-          dayHours: prevState.dayHours + 1 + hourPassed,
-          weekHours: prevState.weekHours + 1 + hourPassed
-        };
-      });
-    }
+    this.props.tickUpdate();
   }
 
   render() {
     return (<main>
-      <button onClick={this.click}>{this.state.name}</button>
+      <button onClick={this.props.handleClick}>{this.props.stateName}</button>
       <div id="record">
         <label htmlFor="dayhours">Horas del día completadas</label>
-        <input readOnly type="number" id="dayhours" value={this.state.dayHours}/><br/>
+        <input readOnly type="number" id="dayhours" value={this.props.dayHours}/><br/>
         <label htmlFor="weekhours">Horas de la semana completadas</label>
-        <input readOnly type="number" id="weekhours" value={this.state.weekHours}/><br/>
+        <input readOnly type="number" id="weekhours" value={this.props.weekHours}/><br/>
         <label htmlFor="remainingtime">Tiempo restante de hora de trabajo</label>
-        <input readOnly type="text" id="remainingtime" value={convertStoM(this.state.remainingTime)}/>
+        <input readOnly type="text" id="remainingtime" value={convertStoM(this.props.remainingTime)}/>
       </div>
     </main>);
   }
--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -5,9 +5,75 @@ import About from '../components/about.jsx';
 import Login from './login.jsx';
 import SingUp from './signup.jsx';
 
+const START = 'Start';
+const PAUSE = 'Pause';
+const CONTINUE = 'Continue';
+let timer;
+
 class HoursTracking extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      stateName: START,
+      dayHours: 0,
+      weekHours: 0,
+      remainingTime: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.setTimer = this.setTimer.bind(this);
+    this.tickUpdate = this.tickUpdate.bind(this);
+  }
+
+  setTimer() {
+    let start = Date.now();
+    timer = setInterval(function () {
+      if (this.state.remainingTime > 0) {
+        const now = Date.now();
+        this.setState(prevState => ({
+          remainingTime: prevState.remainingTime - Math.floor((now - start) / 1000)
+        }));
+        start = now;
+      }
+    }.bind(this), 1000);
+  }
+
+  handleClick() {
+    const { stateName } = this.state;
+    if (stateName === START) {
+      this.setState({ stateName: PAUSE, remainingTime: 60 * 60 });
+      this.setTimer();
+    } else if (stateName === PAUSE) {
+      this.setState({ stateName: CONTINUE });
+      clearInterval(timer);
+    } else if (stateName === CONTINUE) {
+      this.setState({ stateName: PAUSE });
+      this.setTimer();
+    } else {
+      throw 'This error should never happend';
+    }
+  }
+
+  tickUpdate() {
+    const { stateName, remainingTime } = this.state;
+    const aHour = 60 * 60;
+    if (stateName === PAUSE && remainingTime === 0) {
+      this.setState(function (prevState) {
+        return {
+          remainingTime: aHour,
+          dayHours: prevState.dayHours + 1,
+          weekHours: prevState.weekHours + 1
+        };
+      });
+    } else if (stateName === PAUSE && remainingTime < 0) {
+      this.setState(function (prevState) {
+        const hourPassed = Math.floor((-1 * prevState.remainingTime) / aHour);
+        return {
+          remainingTime: hourPassed? aHour * hourPassed + remainingTime: aHour + remainingTime,
+          dayHours: prevState.dayHours + 1 + hourPassed,
+          weekHours: prevState.weekHours + 1 + hourPassed
+        };
+      });
+    }
   }
 
   render() {
@@ -25,7 +91,13 @@ class HoursTracking extends Component {
         </nav>
       </header>
       <div>
-        <Route exact path="/" component={App}/>
+        <Route exact path="/"
+          render={() => <App
+            { ...this.state }
+            handleClick={this.handleClick}
+            tickUpdate={this.tickUpdate}
+          />}
+        />
         <Route path="/about" component={About}/>
         <Route path="/login" component={Login}/>
         <Route path="/signup" component={SingUp}/>
