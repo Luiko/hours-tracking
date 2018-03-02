8bd951f4cf8de9e710a9feaff54dcf457ae22328
Luiko <lokoluis_box@hotmail.com>
2018-03-01 23:17:54 -0700

    add webpack production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+const Path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    filename: 'bundle.js',
+    path: Path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        html5: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    }),
+    new Webpack.optimize.UglifyJsPlugin()
+  ]
+};
