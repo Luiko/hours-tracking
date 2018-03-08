1099736a7865b6f36d58302089fcde35f5e54626
Luiko <lokoluis_box@hotmail.com>
2018-03-08 10:16:37 -0700

    aproach mobile first

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -26,12 +26,12 @@ class App extends Component {
       <h1>Horas Contadas</h1>
       <button aria-label="Botón principal" onClick={this.props.handleClick}>{this.props.btnName}</button>
       <div id="record">
-        <label htmlFor="dayhours">Horas del día completadas</label>
-        <input readOnly type="number" id="dayhours" value={this.props.dayHours}/><br/>
-        <label htmlFor="weekhours">Horas de la semana completadas</label>
-        <input readOnly type="number" id="weekhours" value={this.props.weekHours}/><br/>
-        <label htmlFor="remainingtime">Tiempo restante de hora de trabajo</label>
-        <input readOnly type="text" id="remainingtime" value={convertStoM(this.props.remainingTime)}/>
+        <label className="separate" htmlFor="dayhours">Horas del día completadas</label>
+        <input className="separate" readOnly type="number" id="dayhours" value={this.props.dayHours}/>
+        <label className="separate" htmlFor="weekhours">Horas de la semana completadas</label>
+        <input className="separate" readOnly type="number" id="weekhours" value={this.props.weekHours}/>
+        <label className="separate" htmlFor="remainingtime">Tiempo restante de hora de trabajo</label>
+        <input className="separate" readOnly type="text" id="remainingtime" value={convertStoM(this.props.remainingTime)}/>
       </div>
     </main>);
   }
