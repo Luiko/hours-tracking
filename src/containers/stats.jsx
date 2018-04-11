c0a5732d2722999e22010b95a6c42a0dadd1f99d
Luiko <lokoluis_box@hotmail.com>
2018-04-11 11:58:39 -0600

    reset stats component

--- src/containers/stats.jsx
+++ src/containers/stats.jsx
@@ -6,15 +6,6 @@ class App extends Component {
   }
   render() {
     return (<main>
-      <div id="record">
-      <label htmlFor="dayhours">Horas del día completadas</label>
-      <input type="number" id="dayhours"/>
-      <label htmlFor="weekhours">Horas de la semana completadas</label>
-      <input type="number" id="weekhours"/>
-      <label htmlFor="remainingtime">Tiempo restante de hora de trabajo</label>
-      <input type="time" id="remainingtime"/>
-      </div>
-      <button>Start</button>
     </main>);
   }
 }
