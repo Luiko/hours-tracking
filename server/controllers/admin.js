083b49a72e34872b46dd0e1cc575da1fbb0ecaee
Luiko <lokoluis_box@hotmail.com>
2018-10-18 19:15:47 -0600

    refactor api routes dayhours

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,20 +2,35 @@ const { getWeekStats } = require('../models');
 const moment = require('moment');
 const Joi = require('joi')
 
+async function handler(route, request, h) {
+  const { username, diff } = request.auth.credentials;
+  if (username === process.env.ADMIN) {
+    let date;
+    switch (route) {
+      case 'post_ms':
+        date = moment(request.payload).utcOffset(diff, false);
+      break;
+      case 'get_ms':
+        date = moment(request.params.time).utcOffset(diff, false);
+      break;
+      case 'get_date': {
+        const [year, month, day] = request.params.date.split('/');
+        date = moment(new Date(year, month, day)).utcOffset(diff, false);
+      };
+      break;
+    }
+    const [week] = await getWeekStats(username, date);
+    return week[moment(date).format('dddd').toLowerCase()];
+  }
+  return h.response('').code(401);
+}
+
 exports.register = function (server) {
   const hour = 60 * 60;
   server.route({
     path: '/api/dayhours',
     method: 'POST',
-    async handler(request, h) {
-      const { username, diff } = request.auth.credentials;
-      if (username === process.env.ADMIN) {
-        const date = moment(request.payload).utcOffset(diff, false);
-        const [week] = await getWeekStats(username, date);
-        return week[moment(date).format('dddd').toLowerCase()];
-      }
-      return h.response('').code(401);
-    },
+    handler: handler.bind(null, 'post_ms'),
     options: {
       auth: 'restricted',
       plugins: {
@@ -30,15 +45,7 @@ exports.register = function (server) {
   server.route({
     path: '/api/dayhours/{time}',
     method: 'GET',
-    async handler(request, h) {
-      const { username, diff } = request.auth.credentials;
-      if (username === process.env.ADMIN) {
-        const date = moment(request.params.time).utcOffset(diff, false);
-        const [week] = await getWeekStats(username, date);
-        return week[moment(date).format('dddd').toLowerCase()];
-      }
-      return h.response('').code(401);
-    },
+    handler: handler.bind(null, 'get_ms'),
     options: {
       auth: 'restricted',
       plugins: {
@@ -53,21 +60,12 @@ exports.register = function (server) {
       }
     }
   });
-  const validateDateAPI = 
+  const validateDateAPI =
     /^20[1-2][0-9]\/(1[0-2]|[1-9])\/(3[0-1]|[1-2][0-9]|[1-9])$/;
   server.route({
     path: '/api/dayhours/{date*3}',
     method: 'GET',
-    async handler(request, h) {
-      const [year, month, day] = request.params.date.split('/');
-      const { username, diff } = request.auth.credentials;
-      if (username === process.env.ADMIN) {
-        const date = moment(new Date(year, month, day)).utcOffset(diff, false);
-        const [week] = await getWeekStats(username, date);
-        return week[moment(date).format('dddd').toLowerCase()];
-      }
-      return h.response('').code(401);
-    },
+    handler: handler.bind(null, 'get_date'),
     options: {
       auth: 'restricted',
       plugins: {
